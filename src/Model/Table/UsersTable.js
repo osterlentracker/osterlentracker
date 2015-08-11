9df3110fc54790e279dd119727726f76d2b2035e
Olle Tiinus <tiinusen@cakejs.net>
2015-08-11 02:38:22 +0200

    Added BitwiseType and FlagsBitwiseType

--- src/Model/Table/UsersTable.js
+++ src/Model/Table/UsersTable.js
@@ -6,6 +6,7 @@ export class UsersTable extends Table
 	_initializeSchema(schema)
 	{
 		schema.columnType('options', 'options_bitwise');
+		schema.columnType('flags', 'flags_bitwise');
 		return schema; 
 	}
 }
