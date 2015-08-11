b8b9bea5ecaedaa13ab924a66869928ad8fe13b9
Olle Tiinus <tiinusen@cakejs.net>
2015-08-11 02:38:27 +0200

    Added BitwiseType and FlagsBitwiseType

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -3,3 +3,4 @@ import cakejs from 'cakejs';
 import { Type } from 'Cake/Database/Type';
 
 Type.map('options_bitwise', 'App/Database/Type/OptionsBitwiseType');
+Type.map('flags_bitwise', 'App/Database/Type/FlagsBitwiseType');
