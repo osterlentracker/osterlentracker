0cc5ec8c0f2a28c61d11b5b2372d8cc1fd437cbf
Tiinusen <tiinusen@cakejs.net>
2015-08-28 03:29:52 +0200

    Added travis

--- tests/bootstrap.js
+++ tests/bootstrap.js
@@ -4,5 +4,20 @@ import { SessionManager } from 'Cake/Core/Configure';
 import path from 'path';
 
 require(path.resolve(__dirname,'..','config','bootstrap'));
-
+Configure.write('Datasources', {
+	"default": {
+		"driver": "Mysql",
+		"host": "localhost",
+		"username": "test",
+		"password": "test",
+		"database": "test"
+	},
+	"test": {
+		"driver": "Mysql",
+		"host": "localhost",
+		"username": "test",
+		"password": "test",
+		"database": "test"
+	}
+});
 Configure.write('Web.port', 31337);
