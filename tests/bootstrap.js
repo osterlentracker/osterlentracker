2a35780a4cca42d8d87c8020391ab1cdeadf6375
Tiinusen <tiinusen@cakejs.net>
2015-08-28 03:39:37 +0200

    Fixed config

--- tests/bootstrap.js
+++ tests/bootstrap.js
@@ -1,23 +1,50 @@
 import { Configure } from 'Cake/Core/Configure';
 import { ConnectionManager } from 'Cake/Datasource/ConnectionManager';
 import { SessionManager } from 'Cake/Core/Configure';
-import path from 'path';
 
-require(path.resolve(__dirname,'..','config','bootstrap'));
+import path from 'path';
+import paths from '../config/paths';
+import cakejs from 'cakejs';
+import { Type } from 'Cake/Database/Type';
+
+Type.map('options_bitwise', 'App/Database/Type/OptionsBitwiseType');
+Type.map('flags_bitwise', 'App/Database/Type/FlagsBitwiseType');
+
 Configure.write('Datasources', {
-	"default": {
-		"driver": "Mysql",
-		"host": "localhost",
-		"username": "test",
-		"password": "test",
-		"database": "test"
+	"Web": {
+		"port": 31337,
+		"host": "http://localhost:31337"
 	},
-	"test": {
-		"driver": "Mysql",
-		"host": "localhost",
-		"username": "test",
-		"password": "test",
-		"database": "test"
+	"Datasources": {
+		"default": {
+			"driver": "Mysql",
+			"host": "127.0.0.1",
+			"username": "test",
+			"password": "test",
+			"database": "test"
+		},
+		"test": {
+			"driver": "Mysql",
+			"host": "127.0.0.1",
+			"username": "test",
+			"password": "test",
+			"database": "test"
+		}
+	},
+	"Facebook": {
+		"login": {
+			"id": "",
+			"secret": ""
+		},
+		"feed": {
+			"id": "",
+			"secret": ""
+		}
+	},
+	"SMTP": {
+		"service": "SMTP",
+		"host": "",
+		"port": 25,
+		"ignoreTLS": true
 	}
 });
-Configure.write('Web.port', 31337);
